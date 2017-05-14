c8fa078ee3506a536614b394e3e7513274f85edf
DamDinhDinh <xifurifeng@gmail.com>
2017-05-15 03:46:21 +0700

    Update findresult.component.spec.ts
    Change describe

--- Nhom-Moonlight/project/src/app/findresult.component.spec.ts
+++ Nhom-Moonlight/project/src/app/findresult.component.spec.ts
@@ -5,7 +5,7 @@ import { FTRComponent } from './findresult.component';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 
-describe('AppComponent', () => {
+describe('Find the result test', () => {
   beforeEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     TestBed.configureTestingModule({
